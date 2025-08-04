3ed120dfc2ba31eb2cd656a367486e1c49034595
ghalib-abujafar <112585245+ghalib-abujafar@users.noreply.github.com>
2025-08-04 17:59:36 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -61,14 +61,15 @@ def get_daily_worked_times(driver, month):
     return daily_data
 
 def scrape_web_data(email, password, month, driver):
+    chromedriver_autoinstaller.install()
+
+    # Setup headless options for Render
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
-    chromedriver_autoinstaller.install()
+
     driver = webdriver.Chrome(options=chrome_options)
 
     time.sleep(2)
